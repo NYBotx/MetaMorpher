0a4300ee249c9f1c20476d51ca89d1634885e66f
NYBotx <167092531+NYBotx@users.noreply.github.com>
2024-12-24 18:23:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,20 +6,20 @@ import os
 id_pattern = re.compile(r'^.\d+$')
 
 
-API_ID = os.environ.get("API_ID", "10811400")
-API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7090403697:AAEelXXHhNoWXR_duKpDJiFl39LbI7A3sU0")
-ADMIN = int(os.environ.get("ADMIN", '6469754522'))
-FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "Sunrises24BotUpdates")
-FSUB_GROUP = os.environ.get("FSUB_GROUP", "Sunrises24BotSupport")
-DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://UPLOADXPRO24BOT:UPLOADXPRO24BOT@cluster0.hjfk60f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+API_ID = os.environ.get("API_ID", "13963336")
+API_HASH = os.environ.get("API_HASH", "a144d1e22ef0b29738e8c00713d02678")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8154487335:AAGQFJcKAlbHwylgPHujmjzBv6efv9q0JAA")
+ADMIN = int(os.environ.get("ADMIN", '2103299862'))
+FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "nyyybots")
+FSUB_GROUP = os.environ.get("FSUB_GROUP", "nyyyygrop")
+DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://Nischay999:Nischay999@cluster0.5kufo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 CAPTION = os.environ.get("CAPTION", "")
 group = environ.get('GROUP', '-1002173560131')
 GROUP = int(group) if group and id_pattern.search(group) else None
 #ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
 SUNRISES_PIC= "https://graph.org/file/bd91761f6e938e2e6d23a.jpg"  # Replace with your Telegraph link
-AUTH_USERS = int(os.environ.get("AUTH_USERS", '6469754522'))
+AUTH_USERS = int(os.environ.get("AUTH_USERS", '2103299862'))
 WEBHOOK = bool(os.environ.get("WEBHOOK", True))
 PORT = int(os.environ.get("PORT", "8080"))
-LOG_CHANNEL_ID = os.environ.get("LOG_CHANNEL_ID", -1002145984196)
+LOG_CHANNEL_ID = os.environ.get("LOG_CHANNEL_ID", -1002421238378)
